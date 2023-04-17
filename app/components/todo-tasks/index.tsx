23320c55600a2752775b1c759d2656399b9d29a4
Carlos C. Harris Castillo <ccharris94@gmail.com>
2023-04-17 11:44:45 -0400

    [fix] Minor fixes

--- app/components/todo-tasks/index.tsx
+++ app/components/todo-tasks/index.tsx
@@ -2,20 +2,51 @@
 import { useState } from "react";
 import "./styles.css";
 import { FaCheckCircle, FaMinusCircle } from "react-icons/fa";
-import { RxCrossCircled } from "react-icons/rx";
-import {IoAddOutline } from "react-icons/io5"
+import { IoAddOutline } from "react-icons/io5";
+import { ImBlocked } from "react-icons/im";
+import { SlOptionsVertical } from "react-icons/sl";
+
 type singleTask = {
   content: string;
   status: "Running" | "Canceled" | "Completed";
+  priority: "High" | "Normal" | "Low";
+  category?: string;
 };
 
 export default function TodoTasks() {
   const [tasks, setTasks] = useState<singleTask[]>([]);
-  const [tasksId, setTasksId] = useState(0);
 
+  const setTaskStatus = (id: number, st: "Canceled" | "Completed") => {
+    // if (tasks[id].status == "Running")
+    //   setTasks((tasksArray) => {
+    //     tasksArray[id].status = st
+    //     return tasksArray;
+    //   });
+
+    setTasks(
+      tasks.map((task, index) =>
+        index === id && task.status === "Running"
+          ? { ...task, status: st }
+          : task
+      )
+    );
+  };
+
+  const removeTask = (id: number) => {
+    let result: singleTask[] = [];
+    for (let i = 0; i < tasks.length; i++) {
+      if (id != i) result.push(tasks[i]);
+    }
+    console.log(result);
+    setTasks(result);
+  };
   const addTask = () => {
-    setTasksId(tasksId + 1);
-    const newTask: singleTask = { content: "", status: "Running" };
+    if (tasks.length > 0 && tasks[tasks.length - 1].content == "") return;
+    const newTask: singleTask = {
+      content: "",
+      status: "Running",
+      priority: "Normal",
+    };
     setTasks([...tasks, newTask]);
   };
 
@@ -26,15 +57,24 @@ export default function TodoTasks() {
       <div className="overflow-auto flex flex-col p-3">
         {tasks.map((current, index) => {
           return (
-            <div key={index} className="m-1 p-2 flex flex-row justify-between bg-[#e17572] rounded-[4px] ">
+            <div
+              key={index}
+              className={`m-1 p-2 h-14 flex flex-row justify-between ${
+                current.status === "Running"
+                  ? "bg-[#5191c1]"
+                  : current.status === "Canceled"
+                  ? "bg-[#fb6f24]"
+                  : "bg-[#8ca315]"
+              } rounded-[4px]`}
+            >
               <input
                 placeholder="Task Name"
-                className="p-2 w-[90%] bg-[#e0eebd]"
+                className="p-2 w-[90%] bg-[#e0eebd] rounded-2xl"
                 value={current.content}
                 onChange={(event) =>
                   setTasks(
                     tasks.map((task, i) =>
-                      i === index
+                      i === index && task.status === "Running"
                         ? { ...task, content: event.target.value }
                         : task
                     )
@@ -43,22 +83,39 @@ export default function TodoTasks() {
               />
               <div className="flex">
                 <button
-                  key={index}
-                  className="p-1 m-1 text-[#2b0b16] text-2xl hover:text-green-700"
+                  onClick={() => {
+                    setTaskStatus(index, "Completed");
+                  }}
+                  className={`p-1 m-1 text-[#0a4b75] text-2xl ${
+                    tasks[index].status === "Running"
+                      ? "hover:text-[#8ca315]"
+                      : ""
+                  }`}
                 >
                   <FaCheckCircle />
                 </button>
                 <button
-                  key={index}
-                  className="p-1 m-1 text-[#2b0b16] text-2xl hover:text-red-700"
+                  onClick={() => {
+                    setTaskStatus(index, "Canceled");
+                  }}
+                  className={`p-1 m-1 text-[#0a4b75] text-2xl ${
+                    tasks[index].status === "Running"
+                      ? "hover:text-[#fb6f24]"
+                      : ""
+                  }`}
                 >
-                  <RxCrossCircled />
+                  <ImBlocked />
                 </button>
                 <button
-                  key={index}
-                  className="p-1 m-1 text-[#2b0b16] text-2xl hover:text-gray-800"
+                  onClick={() => {
+                    removeTask(index);
+                  }}
+                  className="p-1 m-1 text-[#0a4b75] text-2xl hover:text-black"
                 >
                   <FaMinusCircle />
+                </button>
+                <button>
+                  <SlOptionsVertical />
                 </button>
               </div>
             </div>
@@ -69,12 +126,24 @@ export default function TodoTasks() {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col ju h-96 w-[600px] bg-[#2b0b16] p-4 shadow-2xl">
-        <button
-          onClick={addTask}
-          className="flex rounded-[4px] bg-[#ce1446] w-10 p-1 m-1 justify-center text-[#405059] text-4xl hover:text-cyan-700">
-          <IoAddOutline />
-        </button>
+      <div className="flex flex-col ju h-96 w-[600px] bg-[#0a4b75] p-4 shadow-2xl">
+        <div className="flex justify-between">
+          <button
+            onClick={addTask}
+            className="flex rounded-[4px] bg-[#1e6495] w-10 p-1 m-1 justify-center text-[#0a4b75] text-4xl hover:text-cyan-700"
+          >
+            <IoAddOutline />
+          </button>
+          <div className="flex self-center">
+            <h1 className="self-center">Filters</h1>
+            <div className="flex m-2">
+              <h1>Category</h1>
+              <select name="category" id="cat">
+                <option value=""> -- Select an option</option>
+              </select>
+            </div>
+          </div>
+        </div>
         {tasksJsx}
       </div>
     </div>
